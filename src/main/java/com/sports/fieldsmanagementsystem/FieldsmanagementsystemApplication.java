5c59b2b90fc5e36d77690399474994b63df37e28
Shubh00796 <shubhamdongare9696@gmail.com>
2025-05-17 23:25:03 +0530

    Initial commit

---
+++ src/main/java/com/sports/fieldsmanagementsystem/FieldsmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.sports.fieldsmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldsmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldsmanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sports/fieldsmanagementsystem/FieldsmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sports.fieldsmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldsmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
